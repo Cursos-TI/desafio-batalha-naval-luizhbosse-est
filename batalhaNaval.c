adb1b416599710330ae069f879fe98f7c80beeb7
Luiz Henrique Bosse <202501077269@alunos.estacio.br>
2025-04-07 00:33:21 +0000

    Batalha naval Mestre - Save

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -49,6 +49,7 @@ int main() {
     }
 
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    /*
     printf("Tabuleiro batalha naval...\n");
     printf("    ");
     for (int c = 0; c < 10; c++)
@@ -64,7 +65,7 @@ int main() {
         } 
         printf("\n");
     }
-
+*/
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
     int habilidadeCone[3][5];
@@ -149,16 +150,167 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
-    // Exibir tabuleiro para o usuário com um monte de 8
+    // Exibir tabuleiro para o usuário com um monte de 2
+    int tabuleiroJogador[10][10];
 
-    // Solicitar para o usuário qual habilidade ele quer usar
+    for(int i = 0; i<10; i++){
+        for(int j = 0; j<10; j++){
+            tabuleiroJogador[i][j] = 2;
+        }         
+    }
 
-    // Solicitar para o usuário a linha
+    int qtdVidas = 10;
+    int acertouTiro = 0;
+    int qtdNavioEncontrado = 0;
 
-    // Solicitar para o usuário a coluna
+    do{
 
-    // Preencher no tabuleiro de 8 do usuário, os valores do tabuleiro, 0 quando agua, 3 quando navio, 8 quando não descoberto
-    // Exibir novamente o tabuleiro e solicitar novamente ataca e posicao, até que todos os navios sejam descobertos, ou que a qtd de vida dele zere
+        printf("\n\nTabuleiro batalha naval...\n");
+        printf("    ");
+        for (int c = 0; c < 10; c++)
+        {    
+            printf("%c ",coluna[c]);    
+        }
+        printf("\n");
+    
+        for(int i = 0; i<10; i++){
+            i==9 ? printf("%d ", i+1): printf("%d  ", i+1);
+            for(int j = 0; j<10; j++){
+                printf(" %d",tabuleiroJogador[i][j]);
+            } 
+            printf("\n");
+        }
+    
+        printf("\n");
+        printf("\n");
+    
+        printf("Quantidade de vidas atual: %d\n", qtdVidas);
+
+        // Solicitar para o usuário qual habilidade ele quer usar
+        int habilidadeSelecionada;
+        int linhaSelecionada;
+        char colunaSelecionada;
+        int colunaSelecionadaInt;
+
+        printf("\n");
+        printf("Indique qual habilidade deseja utilizar:\n");
+        printf("1: Habilidade em Cone\n");
+        printf("2: Habilidade em Cruz\n");
+        printf("3: Habilidade em Octaedro\n");
+        scanf("%d", &habilidadeSelecionada);
+        if (habilidadeSelecionada < 1 || habilidadeSelecionada > 3){
+            printf("Habilidade inválida, tente novamente...\n");
+            //continue;
+        }
+
+        // Solicitar para o usuário a linha
+        printf("Indique em qual linha irá utilizar a habilidade:\n");
+        scanf("%d", &linhaSelecionada);
+        if (linhaSelecionada < 1 || linhaSelecionada > 10){
+            printf("Linha inválida, tente novamente...\n");
+            //continue;
+        }
+
+        // Solicitar para o usuário a coluna
+        printf("Indique em qual coluna irá utilizar a habilidade:\n");
+        scanf(" %c", &colunaSelecionada);
+        colunaSelecionadaInt = 0;
+        switch (colunaSelecionada)
+        {
+            case 'A':
+                colunaSelecionadaInt = 1;
+                break;
+            case 'B':
+                colunaSelecionadaInt = 2;
+                break;
+            case 'C':
+                colunaSelecionadaInt = 3;
+                break;
+            case 'D':
+                colunaSelecionadaInt = 4;
+                break;
+            case 'E':
+                colunaSelecionadaInt = 5;
+                break;
+            case 'F':
+                colunaSelecionadaInt = 6;
+                break;
+            case 'G':
+                colunaSelecionadaInt = 7;
+                break;
+            case 'H':
+                colunaSelecionadaInt = 8;
+                break;
+            case 'I':
+                colunaSelecionadaInt = 9;
+                break;
+            case 'J':
+                colunaSelecionadaInt = 10;
+                break;
+        
+        default:
+        printf("Coluna inválida, tente novamente...\n");
+            break;
+        }
+
+        // Decrementa 1 para não precisar ficar fazendo sempre -1 nos indexadores
+        colunaSelecionadaInt--;
+        linhaSelecionada--;
+
+        // Preencher no tabuleiro de 8 do usuário, os valores do tabuleiro, 0 quando agua, 3 quando navio, 8 quando não descoberto
+        for (int i=0; i < 10; i++)
+        {
+            for (int j=0; j < 10; j++){
+                // Verifica se a posição deve está dentro da matriz 3x5 de habilidade
+                if (i>=linhaSelecionada && i<=linhaSelecionada+2 && j >= colunaSelecionadaInt && j <= colunaSelecionadaInt +4){ 
+                    
+                    // Fazer IF, de acordo com o tipo de habilidade selecionada, para não verificar toda a matriz 3x5
+                    switch (habilidadeSelecionada)
+                    {
+                        case 1: // Cone
+                            /* code */
+                            break;
+                        case 2: // Cruz
+                            /* code */
+                            break;
+                        case 3: // Octaedro
+                            /* code */
+                            break;
+                    
+                    default:
+                        break;
+                    }
+                    tabuleiroJogador[i][j] = tabuleiro[i][j];
+
+                    if(tabuleiro[i][j] == 3){
+                        acertouTiro = 1;
+                        qtdNavioEncontrado++;
+                    }
+
+                }
+            }
+        }
+
+        if (acertouTiro == 0){
+            qtdVidas--;
+        }
+        acertouTiro = 0;
+
+        if (qtdNavioEncontrado == 12){
+            break;
+        }
+    
+    } while(qtdVidas > 0);
+
+    printf("Jogo encerrado..\n");
+
+    if (qtdVidas == 0){
+        printf("Você perdeu... Tente novamente...\n");  
+    }
+
+    if (qtdNavioEncontrado == 12){
+        printf("Parabéns... Você venceu\n");  
+    }
 
     return 0;
 }
